aa3db280c93a4a339082c9eadadf5d4695464eae
TilGP <d21pockrandtti>
2024-10-23 08:04:54 +0200

    added loading module button

--- src/main.py
+++ src/main.py
@@ -1,9 +1,45 @@
 import pathlib
+import pkgutil
 import sys
 import types
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton
 import importlib.util
+
+
+class ModulesWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        layout = QVBoxLayout(self)
+        self.__modules_path = pathlib.Path(__file__).resolve().parent / pathlib.Path('modules') / pathlib.Path(
+            '__init__.py')
+        self.__available_modules = self.__get_available_modules()
+
+        for module_name in self.__available_modules:
+            button = QPushButton(text=module_name, parent=self)
+            button.clicked.connect(lambda checked: self.__load_module(module_name=module_name))
+            layout.addWidget(button)
+
+    def __load_module(self, module_name: str) -> types.ModuleType:
+        """
+        Import a module at runtime from a directory.
+        :param module_name: the name of the module to load
+        :return: the loaded module
+        """
+        try:
+            spec = importlib.util.spec_from_file_location(module_name, str(self.__modules_path))
+            module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(module)
+            sys.modules[module_name] = module
+            print(f'loaded module: {module_name}')
+            return module
+        except ImportError as e:
+            print(f'failed to load module {module_name}: {e}')
+
+    def __get_available_modules(self) -> list[str]:
+        modules = self.__load_module('modules')
+        return [name for _, name, _ in pkgutil.iter_modules(modules.__path__)]
 
 
 class MainWindow(QMainWindow):
@@ -11,22 +47,9 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         # Hauptlayout des Fensters
-        self.central_widget = QWidget()
+        self.central_widget = ModulesWidget(parent=self)
         self.layout = QVBoxLayout(self.central_widget)
         self.setCentralWidget(self.central_widget)
-
-    def load_module_from_anywhere(self, path: pathlib.Path, module_name: str) -> types.ModuleType:
-        """
-        Import a module at runtime from a directory.
-        :param path: the modules directory
-        :param module_name: the name of the module to load
-        :return: the loaded module
-        """
-        spec = importlib.util.spec_from_file_location(module_name, str(path))
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        sys.modules[module_name] = module
-        return module
 
 
 if __name__ == '__main__':
@@ -35,11 +58,5 @@ if __name__ == '__main__':
     # Erstelle das Hauptfenster
     window = MainWindow()
 
-    # Modul dynamisch laden und zum Layout hinzufügen
-    modules_path = pathlib.Path(__file__).resolve().parent / pathlib.Path('modules') / pathlib.Path('__init__.py')
-    example_mod = window.load_module_from_anywhere(path=modules_path, module_name='modules')
-    widget = example_mod.hello_world.HelloWorldWidget()
-    window.setCentralWidget(widget)
-
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
