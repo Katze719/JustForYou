715f2ef150ee489cb0c2dd4ac021357638d9a449
Katze719 <pauldorn1234@gmail.com>
2025-03-24 14:28:25 +0100

    better str history format

--- src/helpers/historyManager.py
+++ src/helpers/historyManager.py
@@ -2,10 +2,10 @@ import json
 import pathlib
 from typing import List, Dict, Any
 from src.helpers import AES
-
+import datetime
 
 class HistoryManager:
-    def __init__(self, history_file: str):
+    def __init__(self, history_file: str = "main-history.bin"):
         self.historyFile = pathlib.Path(history_file)
         if not self.historyFile.exists():
             self._save([])  # Falls Datei nicht existiert, leere Liste speichern
@@ -21,15 +21,25 @@ class HistoryManager:
         except (json.JSONDecodeError, FileNotFoundError):
             return []  # Falls Datei leer oder defekt ist
 
-    def add_entry(self, expression: str, result: Any):
+    def add_entry(self, expression: str, result: Any, date: datetime.datetime = None):
         """Fügt eine neue Rechnung zur History hinzu."""
+        if date is None:
+            date = datetime.datetime.now()
         history = self._load()
-        history.append({"expression": expression, "result": result})
+        history.append({"expression": expression, "result": result, "date": date.strftime("%Y-%m-%d %H:%M:%S")})
         self._save(history)
 
     def get_history(self) -> List[Dict[str, Any]]:
         """Gibt die gespeicherte Rechnungs-History zurück."""
         return self._load()
+
+    def get_history_fmt(self) -> str:
+        data = self._load()
+        data_fmt = [f"{row["date"]} E: {row["expression"]} --> {row["result"]}\n" for row in data]
+        result = ""
+        for d in data_fmt:
+            result += d
+        return result
 
     def clear_history(self):
         """Löscht die gesamte Rechnungs-History."""
