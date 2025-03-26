f7fcffac850d2b1dde8d653f3f743f00a73c4fb4
Spaex01 <hugo.machill@gmail.com>
2025-03-26 09:58:01 +0100

    Mathe funktion wurzel und facultät implementiert oder sowas digga

--- src/helpers/parser.py
+++ src/helpers/parser.py
@@ -1,13 +1,15 @@
 import re
+import math
 
 class CalculatorParser:
     def __init__(self):
-        self.valid_pattern = re.compile(r"^[0-9+\-*/(). ]+$")
+        self.valid_pattern = re.compile(r"^[0-9+\-*/(). ]+|math\.sqrt\([0-9a-zA-Z\.\-\+\*\/\(\) ]*\)$")
 
     def parse_and_calculate(self, expression: str) -> str:
         if not self.valid_pattern.match(expression):
             return "Ungültiger Ausdruck"
         try:
+            print(f'parsing: {expression}')
             result = eval(expression)
             return str(result)
         except Exception as e:
